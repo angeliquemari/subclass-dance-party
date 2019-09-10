4887f151fc0632e03ccca186229c54347f0e88cc
angeliquemari <angelique@nehmzow.com>
2019-09-09 20:10:06 -0700

    added skeleton for rainbowDancer and squareDancer as well as updating html file to add functionality for rainbow and squaredancer

---
+++ spec/squareDancer.js
@@ -0,0 +1,35 @@
+describe('squareDancer', function() {
+
+  var squareDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    squareDancer = new SquareDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(squareDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  xit('should have a step function that makes its node change location in a square pattern', function() {
+    // sinon.spy(squareDancer.$node, 'toggle');
+    // squareDancer.step();
+
+    // expect(squareDancer.$node.pickRandomColor.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(squareDancer, 'step');
+      expect(squareDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(squareDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(squareDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
---
+++ src/squareDancer.js
@@ -0,0 +1,17 @@
+var SquareDancer = function(top, left, timeBetweenSteps) {
+  Dancer.apply(this, arguments);
+};
+
+SquareDancer.prototype = Object.create(Dancer.prototype);
+SquareDancer.prototype.constructor = SquareDancer;
+
+SquareDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+
+
+  //this.$node.rotateClockwise90AndJump5Px();
+};
